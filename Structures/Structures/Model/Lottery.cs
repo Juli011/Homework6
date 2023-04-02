ca543e45fed1de454b235e8001721784e24d8798
uuusss631@gmail.com <uuusss631@gmail.com>
2023-04-02 09:44:27 +0300

    update

--- Structures/Structures/Model/Lottery.cs
+++ Structures/Structures/Model/Lottery.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System.Collections;
 
 namespace Structures.Model
 {
     internal static class Lottery
     {
-        public static List<int> raffleAll = new List<int>();
+        public static HashSet<int> raffleAll = new HashSet<int>();
         public static int Number;
-        
-        public static HashSet<int> Emit(int number, int N)
+
+        public static List<int> Emit(int number, int N)
         {
             Number = number;
             raffleAll.Clear();
-            Random rnd = new Random();  
-            for(int i = 0; i < N; i++)
+
+            Random rnd = new Random();
+            while (raffleAll.Count != N)
             {
                 raffleAll.Add(rnd.Next(1, number));
             }
-            return new HashSet<int>(raffleAll);
+            List<int> raff = new List<int>();   
+            raff.AddRange(raffleAll);
+            return raff;
         }
     }
 }
